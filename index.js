17fef2772e58d0bbc439c76d7557051cd7356099
josé felipe <146544258+eelipe@users.noreply.github.com>
2025-04-08 16:34:43 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize('aula', 'postgres', 'postgres', {
+  host: 'localhost',
+  dialect: 'postgres' 
+});
+conectar();
+
+async function conectar(){
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+}
